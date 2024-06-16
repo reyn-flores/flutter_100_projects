d1bbbf7ecc1a559cc6697bab4afb46bbe50b2cef
Reyn Flores <reynold.flores@contenthouse.io>
2024-06-16 21:32:54 +0800

    Create a countdown timer with TweenAnimationBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,129 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Countdown timer with Tween',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CountdownTimer(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CountdownTimer extends StatefulWidget {
+  const CountdownTimer({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CountdownTimer> createState() => _CountdownTimerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _CountdownTimerState extends State<CountdownTimer> {
+  bool isPaused = true;
+  Duration duration = const Duration(seconds: 60);
+  late Duration runningDuration = duration;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          mainAxisSize: MainAxisSize.min,
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              '1 minute countdown timer',
+              style: TextStyle(
+                fontSize: 20,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(
+              height: 32.0,
+            ),
+            if (isPaused) ...[
+              _buildTimer(runningDuration),
+            ] else ...[
+              TweenAnimationBuilder(
+                tween: Tween(
+                  begin: runningDuration,
+                  end: const Duration(milliseconds: 0),
+                ),
+                duration: runningDuration,
+                builder: (context, value, child) {
+                  runningDuration = value;
+                  return _buildTimer(value);
+                },
+                onEnd: () {
+                  setState(() {
+                    isPaused = true;
+                    duration = const Duration(seconds: 60);
+                    runningDuration = duration;
+                  });
+                },
+              ),
+            ],
+            const SizedBox(
+              height: 32.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isPaused = !isPaused;
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(30),
+                backgroundColor: isPaused
+                    ? Theme.of(context).colorScheme.primary
+                    : Theme.of(context).colorScheme.onPrimary,
+              ),
+              child: Text(
+                isPaused ? 'Start' : 'Pause',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: isPaused
+                      ? Theme.of(context).colorScheme.onPrimary
+                      : Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildTimer(Duration value) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(
+          height: 275,
+          width: 275,
+          child: CircularProgressIndicator(
+            backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+            strokeCap: StrokeCap.round,
+            strokeWidth: 12.0,
+            value: value.inMilliseconds / duration.inMilliseconds,
+          ),
+        ),
+        Text(
+          _formatDuration(value),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 32.0,
+          ),
+        )
+      ],
+    );
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$minutes:$seconds";
   }
 }
