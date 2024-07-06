a607c4c7d01502bb1ff15d1e4d5abd948c8af972
Reyn Flores <85285341+reyn-flores@users.noreply.github.com>
2024-07-06 21:21:01 +0800

    Toggle from light and dark theme with stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,72 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Click on the icon button or the elevated button'),
+              const SizedBox(
+                height: 24,
+              ),
+              IconButton(
+                onPressed: () {
+                  _toggleThemeMode();
+                },
+                icon: Icon(
+                  _isLightMode() ? Icons.dark_mode : Icons.light_mode,
+                  size: 36,
+                ),
+              ),
+              Text('You are in ${_isLightMode() ? 'light' : 'dark'} mode'),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _toggleThemeMode();
+                },
+                child: Text('Enter ${_isLightMode() ? 'dark' : 'light'} mode'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  bool _isLightMode() {
+    return _themeMode != ThemeMode.dark;
+  }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  void _toggleThemeMode() {
+    setState(() {
+      if (_isLightMode()) {
+        _themeMode = ThemeMode.dark;
+      } else {
+        _themeMode = ThemeMode.light;
+      }
+    });
   }
 }
