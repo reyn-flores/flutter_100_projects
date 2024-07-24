c6f43e31b8ffc54eacdde1800284306847e29084
Reyn Flores <85285341+reyn-flores@users.noreply.github.com>
2024-07-24 21:37:13 +0800

    Login feature made with flutter bloc

---
+++ lib/login/login.dart
@@ -0,0 +1,4 @@
+export 'models/models.dart';
+export 'bloc/login_bloc.dart';
+export 'view/login_page.dart';
+export 'view/login_form.dart';
---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_100_projects/login/login.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute(builder: (_) => const LoginPage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: BlocProvider(
+          create: (context) {
+            return LoginBloc(
+              authenticationRepository:
+                  RepositoryProvider.of<AuthenticationRepository>(context),
+            );
+          },
+          child: const LoginForm(),
+        ),
+      ),
+    );
+  }
+}
