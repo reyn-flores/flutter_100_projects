f2717a3c4aedc75a9a86d988d1d6419b4120c077
Reyn Flores <85285341+reyn-flores@users.noreply.github.com>
2024-07-28 18:45:23 +0800

    Create a custom loading animation with AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,68 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.linear)
+          ..addStatusListener(
+            (status) {
+              if (status == AnimationStatus.completed) {
+                animationController.reverse();
+              } else if (status == AnimationStatus.dismissed) {
+                animationController.forward();
+              }
+            },
+          );
+    animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: LoadingAnimation(animation: animation),
+    );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+}
+
+class LoadingAnimation extends AnimatedWidget {
+  const LoadingAnimation({super.key, required Animation<double> animation})
+      : super(listenable: animation);
+
+  static final _opacityTween = Tween<double>(begin: 0.5, end: 1);
+  static final _sizeTween = Tween<double>(begin: 10, end: 50);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
   }
 }
