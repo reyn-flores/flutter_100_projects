909eb58306b4a70bcea4410605581e378968fbcd
Reyn Flores <85285341+reyn-flores@users.noreply.github.com>
2024-08-05 20:57:47 +0800

    Implement mulitple image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,8 +31,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+  List<File> selectedImages = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: selectedImages.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Image.file(
+                        selectedImages[index],
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    getImages();
+                  },
+                  child: const Text('Select images from gallery'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future getImages() async {
+    try {
+      final List<XFile> pickedFileList = await _picker.pickMultiImage();
+      if (pickedFileList.isEmpty) {
+        _showSnackBar('Nothing was selected');
+        return;
+      }
+
+      for (final file in pickedFileList) {
+        selectedImages.add(File(file.path));
+      }
+
+      setState(() {});
+    } catch (e) {
+      _showSnackBar(e.toString());
+    }
+  }
+
+  _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text(message),
+      ),
+    );
   }
 }
